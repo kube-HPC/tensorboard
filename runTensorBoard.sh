93b0b466254fc1a789cfe62ba4490f6e98b077e2
golanha <golanha@rnd-hub.com>
2019-12-31 11:19:25 +0200

    Support fs storage

--- runTensorBoard.sh
+++ runTensorBoard.sh
@@ -1,24 +1,27 @@
-credentials_file="/root/.aws/credentials"
-if [[ !(-z "${AWS_ACCESS_KEY_ID}") ]]; then
-  if [[ !(-z "${AWS_SECRET_ACCESS_KEY}") ]]; then
-    mkdir /root/.aws
-    
-    echo [default] > ${credentials_file}
-    echo aws_access_key_id=${AWS_ACCESS_KEY_ID}>>${credentials_file}
-    echo aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}>>${credentials_file}
-    if [[ !(-z "${logDir}") ]]; then
-      export S3_ENDPOINT=`echo ${S3_ENDPOINT_URL}|sed 's/https\?:\/\///'`
-      if [[ ${S3_ENDPOINT_URL} == https* ]]; then
-         export S3_USE_HTTPS="1"
-      else
-         export S3_USE_HTTPS="0" 
-      fi
-      python /usr/local/lib/python3.6/dist-packages/tensorboard/main.py --logdir ${logDir} --bind_all
+if [[ (-z "${logDir}") ]]; then
+    echo "Must supply logs directory."
+else
+    if [[ "${DEFAULT_STORAGE}" == "fs" ]]; then 
+        python /usr/local/lib/python3.6/dist-packages/tensorboard/main.py --logdir ${logDir} --bind_all
     else
-      echo "Must supply logs directory."
-    fi 
-  fi
-fi
-if  ! test -f "$credentials_file"; then
-   echo "Must supply S3 credentials: aws_access_key_id, aws_secret_access_key"
+        credentials_file="/root/.aws/credentials"
+        if [[ !(-z "${AWS_ACCESS_KEY_ID}") ]]; then
+            if [[ !(-z "${AWS_SECRET_ACCESS_KEY}") ]]; then
+                mkdir /root/.aws
+                echo [default] > ${credentials_file}
+                echo aws_access_key_id=${AWS_ACCESS_KEY_ID}>>${credentials_file}
+                echo aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}>>${credentials_file}
+                export S3_ENDPOINT=`echo ${S3_ENDPOINT_URL}|sed 's/https\?:\/\///'`
+                if [[ ${S3_ENDPOINT_URL} == https* ]]; then
+                    export S3_USE_HTTPS="1"
+                else
+                    export S3_USE_HTTPS="0"  
+                fi
+                python /usr/local/lib/python3.6/dist-packages/tensorboard/main.py --logdir ${logDir} --bind_all
+            fi
+        fi
+        if  ! test -f "$credentials_file"; then
+            echo "Must supply S3 credentials: aws_access_key_id, aws_secret_access_key"
+        fi
+    fi
 fi
